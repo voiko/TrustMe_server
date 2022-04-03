84c7b95e931b51d4f2c2a0c052b179140a55e3de
Voiko <nadavv10@walla.com>
2022-04-03 22:35:25 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const {dbConfig} = require('./config/db');
+// const userRoutes = require('./routes/user');
+const mongoose  = require('mongoose');
+
+app.listen(PORT,async (err)=>{
+    if(err){
+        console.error(err);
+        return;
+
+    }
+    try{
+            const connection = await mongoose.connect(dbConfig.uri,{
+                    useNewUrlParser: true,
+                    useUnifiedTopology: true
+            })
+            console.log("Server and mongo are  up!");
+    } catch(err){
+        console.log(err);
+    }
+   
+
+})
---
+++ config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+    dbConfig:{
+        uri:'mongodb+srv://voiko:Aa123456@cluster0.ihnqp.mongodb.net/TrustMe?retryWrites=true&w=majority'
+    }
+}
