7ded0e4b51ecbbe04283bd880d0a431a086bdd51
Or Kravitz <imorkravitz@gmail.com>
2022-04-04 10:34:24 +0300

    server

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const {dbConfig} = require('./config/db');
+const {
+    dbConfig
+} = require('./config/db');
 // const userRoutes = require('./routes/user');
-const mongoose  = require('mongoose');
+const mongoose = require('mongoose');
 
-app.listen(PORT,async (err)=>{
-    if(err){
+app.listen(PORT, async (err) => {
+    if (err) {
         console.error(err);
         return;
-
     }
-    try{
-            const connection = await mongoose.connect(dbConfig.uri,{
-                    useNewUrlParser: true,
-                    useUnifiedTopology: true
-            })
-            console.log("Server and mongo are  up!");
-    } catch(err){
+    try {
+        const connection = await mongoose.connect(dbConfig.uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        console.log("Server and mongoDB is up!");
+    } catch (err) {
         console.log(err);
     }
-   
-
 })
