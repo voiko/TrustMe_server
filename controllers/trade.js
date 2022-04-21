5f1d9d753101def19651f1a7a64b57d8e250365b
Or Kravitz <imorkravitz@gmail.com>
2022-04-21 17:55:51 +0300

    fix server bug

--- controllers/trade.js
+++ controllers/trade.js
@@ -8,41 +8,40 @@ const authenticate = require('../common/auth_middleware') //this virable we will
 // 
 
 const sendError = (res, code, message) => {
-    return res.status(code).send({
-        'status': 'failed',
-        'error': message
-    })
+  return res.status(code).send({
+    'status': 'failed',
+    'error': message
+  })
 }
 
 const add = (req, res, next) => {
-    const contract = new Trade({
-        side: req.body.side,
-        description: req.body.description,
-        deposit: req.body.deposit,
-        emailOfAnotherSide: req.body.emailOfAnotherSide,
-        date: req.body.date
-      });
-      contract.save().then((result)=>{
-        res.status(201).json({
-          message : 'contract added successfully',
-          contractId : result.id
-        });
-      })
-    
-      console.log(contract);
+  const contract = new Trade({
+    side: req.body.side,
+    description: req.body.description,
+    deposit: req.body.deposit,
+    emailOfAnotherSide: req.body.emailOfAnotherSide,
+    date: req.body.date
+  });
+  contract.save().then((result) => {
+    res.status(201).json({
+      message: 'contract added successfully',
+      contractId: result.id
+    });
+  })
+
+  console.log(contract);
 }
 
 const getContract = async (req, res) => {
-    console.log("get contract1")
-    Trade.find().then(documents =>{
-        res.status(200).json({
-          message : 'posts fetched successfully',
-          contracts: documents
-        });
+  Trade.find().then(documents => {
+    res.status(200).json({
+      message: 'posts fetched successfully',
+      contracts: documents
     });
+  });
 }
 
 module.exports = {
-    add,
-    getContract
+  add,
+  getContract
 }
